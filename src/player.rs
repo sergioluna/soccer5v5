e35b6ccaf373c6710849a86264c5b1d9824297e5
sergioluna <sluna@sergioluna.dev>
2024-08-08 22:21:53 -0700

    Add player tests

--- src/player.rs
+++ src/player.rs
@@ -34,4 +34,31 @@ impl Player {
 }
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use super::*;
+
+    #[test]
+    fn get_name() {
+        let player = Player::new(String::from("Washington"), 1);
+        assert_eq!(*player.name(), String::from("Washington"));
+    }
+
+    #[test]
+    fn get_number() {
+        let player = Player::new(String::from("Washington"), 1);
+        assert_eq!(player.number(), 1);
+    }
+
+    #[test]
+    fn get_position() {
+        let player = Player::new(String::from("Washington"), 1);
+        assert_eq!(player.position(), None);
+    }
+
+    #[test]
+    fn set_position() {
+        let mut player = Player::new(String::from("Washington"), 1);
+        player.set_position(PlayerPosition::GK);
+        assert_eq!(player.position(), Some(PlayerPosition::GK));
+    }
+}
